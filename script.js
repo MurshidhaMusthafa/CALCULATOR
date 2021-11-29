c3603a7147e51ccafd23afa99ee83029d5057160
Murshidha <missbosss6076@gmail.com>
2021-11-29 23:50:25 +0530

    initialcommmit

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ var displayValue=(num)=>result.value+=num
 var clearValue=()=>result.value=""
 
 var evaluateExpression=()=>result.value=eval(result.value)
+
+var backspace=()=>result.value=result.value.slice(0,-1)
